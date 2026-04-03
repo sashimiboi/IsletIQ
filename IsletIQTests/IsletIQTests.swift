1d2942480e359b901cfc628fe8640594c3bbcbd2
sashimiboi <53438741+sashimiboi@users.noreply.github.com>
2026-04-03 10:29:42 -0500

    Initial Commit

---
+++ IsletIQTests/IsletIQTests.swift
@@ -0,0 +1,18 @@
+//
+//  IsletIQTests.swift
+//  IsletIQTests
+//
+//  Created by Anthony Loya on 4/3/26.
+//
+
+import Testing
+
+struct IsletIQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
